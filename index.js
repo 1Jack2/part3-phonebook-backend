5e8d8e87e88d1b20740d616cfe3243c0f298347f
Jack Zhang <jack333zhang@gmail.com>
2022-11-26 16:50:07 +0800

    Part3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -61,35 +61,31 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-// const generateId = () => {
-//     const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//     return maxId + 1
-// }
+const generateId = () => {
+    return Math.floor(10000 * Math.random())
+}
 
-// app.post('/api/notes', (request, response) => {
-//
-//     const body = request.body
-//
-//     if (!body.content) {
-//         return response.status(400).json({
-//             error: 'content missing'
-//         })
-//     }
-//
-//     const note = {
-//         content: body.content,
-//         important: body.important || false,
-//         data: new Date(),
-//         id: generateId()
-//     }
-//
-//     notes = notes.concat(note)
-//
-//     console.log(note);
-//     response.json(note)
-// })
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+
+    // if (!body.content) {
+    //     return response.status(400).json({
+    //         error: 'content missing'
+    //     })
+    // }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    console.log(person);
+    response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
