a8ad843a917bbf002e59a99f1138c4ee416976af
Jack Zhang <jack333zhang@gmail.com>
2022-11-27 22:15:08 +0800

    Part3.19-3.21

--- models/person.js
+++ models/person.js
@@ -11,9 +11,23 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: v => {
+                return /^\d{3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        }
+    }
 })
+
 personSchema.set('toJSON', {
     transform: (document, returnObject) => {
         returnObject.id = returnObject._id.toString()
