c1a6434b63de0b7b6dbf48919e60afd24b5e0a8d
Jack Zhang <jack333zhang@gmail.com>
2022-11-27 17:07:15 +0800

    Part3.9-3.14

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
 
 const app = express()
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const Person = require('./models/person')
 
 let persons = [
     {
@@ -42,21 +48,25 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    console.log('############')
+    Person.find({}).then(persons => {
+        response.json(persons)
+    }).catch(error => {
+        console.error(error);
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => {
-        console.log(person.id, typeof person.id, id, typeof id, person.id === id);
-        return person.id === id
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        console.log(error);
+        response.status(400).send({error: 'malformatted id'})
     })
-    if (person) {
-        response.json(person)
-    } else {
-        console.log("missing")
-        response.status(404).end()
-    }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -66,40 +76,33 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    return Math.floor(10000 * Math.random())
-}
-
 app.post('/api/persons', (request, response) => {
-
     const body = request.body
 
-    if (!body.name) {
+    if (body.name === undefined) {
         return response.status(400).json({
             error: 'name missing'
         })
     }
-    if (!body.number) {
+    if (body.number === undefined) {
         return response.status(400).json({
             error: 'number missing'
         })
     }
-    if (persons.some((person) => person.name === body.name)) {
-        return response.status(400).json({
-            error: `${body.name} already exist`
-        })
-    }
+    // if (persons.some((person) => person.name === body.name)) {
+    //     return response.status(400).json({
+    //         error: `${body.name} already exist`
+    //     })
+    // }
 
-    const person = {
+    const person = new Person ({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-
-    console.log(person);
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT || 3001
