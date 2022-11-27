17e5f5edaf13a551cfe05b7b19bb488c6a674307
Jack Zhang <jack333zhang@gmail.com>
2022-11-27 20:54:10 +0800

    Part3.15-3.18

--- index.js
+++ index.js
@@ -14,41 +14,21 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 const Person = require('./models/person')
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<p> Phonebook has info for ${persons.length} people </p> \n`
-        + `<p> ${new Date()} </p>`)
+    Person.find({})
+        .then(result => {
+            response.send(
+                `<p> Phonebook has info for ${result.length} people </p> \n`
+                + `<p> ${new Date()} </p>`)
+        })
+        .catch(error => console.log(error))
 })
 
 app.get('/api/persons', (request, response) => {
-    console.log('############')
     Person.find({}).then(persons => {
         response.json(persons)
     }).catch(error => {
@@ -56,24 +36,39 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    }).catch(error => {
-        console.log(error);
-        response.status(400).send({error: 'malformatted id'})
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    // original document without the modifications.
+    const person = {
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -89,13 +84,8 @@ app.post('/api/persons', (request, response) => {
             error: 'number missing'
         })
     }
-    // if (persons.some((person) => person.name === body.name)) {
-    //     return response.status(400).json({
-    //         error: `${body.name} already exist`
-    //     })
-    // }
 
-    const person = new Person ({
+    const person = new Person({
         name: body.name,
         number: body.number,
     })
@@ -104,6 +94,19 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
